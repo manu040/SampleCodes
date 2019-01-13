f3e5cd7e350e5f65ad5e9842c2db0fd7a9cd6597
manu040 <46649987+manu040@users.noreply.github.com>
2019-01-13 20:45:46 +0530

    Added the user name also

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld{
 public static void main(String[] args){
 
 System.out.println("This is a Sample Hello World Program");
-
+  
+//Added printing the user name also
+System.out.println("Manoj is the user")
 }
 }
